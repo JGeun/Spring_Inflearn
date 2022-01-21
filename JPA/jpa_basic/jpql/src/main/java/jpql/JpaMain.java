e9e1680b88e1f8f29294d13f937e5908fb6854b5
JGeun <pjk8926@gmail.com>
2022-01-21 22:30:48 +0900

    Study fetch join

--- JPA/jpa_basic/jpql/src/main/java/jpql/JpaMain.java
+++ JPA/jpa_basic/jpql/src/main/java/jpql/JpaMain.java
@@ -4,6 +4,8 @@ import javax.persistence.*;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class JpaMain {
@@ -16,23 +18,40 @@ public class JpaMain {
         tx.begin();
 
         try {
+            Team teamA = new Team();
+            teamA.setName("팀A");
+            em.persist(teamA);
+
+            Team teamB = new Team();
+            teamB.setName("팀A");
+            em.persist(teamB);
+
             Member member1 = new Member();
-            member1.setUsername("관리자1");
+            member1.setUsername("회원1");
+            member1.setTeam(teamA);
             em.persist(member1);
 
             Member member2 = new Member();
-            member2.setUsername("관리자1");
+            member2.setUsername("회원2");
+            member2.setTeam(teamA);
             em.persist(member2);
+
+            Member member3 = new Member();
+            member3.setUsername("회원2");
+            member3.setTeam(teamB);
+            em.persist(member3);
 
             em.flush();
             em.clear();
 
-            String query = "select function('group_concat', m.username) From Member m";
+            String query = "select distinct t from Team t join fetch t.members";
 
-            List<String> result = em.createQuery(query, String.class).getResultList();
-
-            for (String s : result) {
-                System.out.println("s = " + s);
+            List<Team> result = em.createQuery(query, Team.class).getResultList();
+            for (Team team : result) {
+                System.out.println("team = " + team.getName() + "|" + team.getMembers().size());
+                for(Member member : team.getMembers()){
+                    System.out.println("-> member = " + member);
+                }
             }
 
             tx.commit();
