4fbd761439b3a3313ef2a2d039c99b17474a200d
JGeun <pjk8926@gmail.com>
2022-01-26 00:30:32 +0900

    finish domain

--- JPA/jpa_practice/jpashop/src/main/java/jpabook/jpashop/domain/Category.java
+++ JPA/jpa_practice/jpashop/src/main/java/jpabook/jpashop/domain/Category.java
@@ -18,12 +18,16 @@ public class Category {
 
     private String name;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "parent_id")
     private Category parent;
 
     @OneToMany(mappedBy = "parent")
     private List<Category> child = new ArrayList<>();
 
-
+    //==연관관계 메서드==//
+    public void addChildCategory(Category child){
+        this.child.add(child);
+        child.setParent(this);
+    }
 }
