7595fc3c8edb7c829c10e883cf7898261683ed75
JGeun <pjk8926@gmail.com>
2022-01-26 20:41:14 +0900

    make member service and test

---
+++ JPA/jpa_practice/jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,45 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    //회원 가입
+    @Transactional
+    public Long join(Member member){
+        validateDuplicateMember(member); //중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        //멀티스레드 조건에서 이름의 중복을 방지하기 위한 최후의 조건으로 member의 이름을 unique조건으로
+        //달아주는 것이 좋음
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if(!findMembers.isEmpty()){
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    //회원 전체 조회
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(Long memberId){
+        return memberRepository.findOne(memberId);
+    }
+}
